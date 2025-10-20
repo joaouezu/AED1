ea34b19c78d5bdefa939bc286b3327a12a5fe21f
joaouezu <joao.luz07@unifesp.br>
2025-10-20 13:49:12 -0300

    Update 1068b.c

--- 1068b.c
+++ 1068b.c
@@ -1,59 +1,29 @@
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <string.h>
-    typedef struct no {
-        char valor;
-        struct no *prox;
-    }TNo;
-    typedef TNo *No;
-    int push (No lst, char info) {
-        No novo = malloc(sizeof(TNo));
-        novo->valor = info;
-        novo->prox = lst->prox;
-        lst->prox = novo;
-        return 1;
-    }
-    int pop (No lst) {
-        if (lst->prox == NULL) {
-            return 0;
-        }
-        No lixo = lst->prox;
-        lst->prox = lixo->prox;
-        free (lixo);
-        return 1;
-    }
-    void liberar(No lst) {
-        No aux = lst->prox;
-        while (aux != NULL) {
-            No auxprox = aux->prox;
-            free(aux);
-            aux = auxprox;
-        }
-    }
-    int main () {
-        int i;
-        char string[100];
-        No lst = malloc(sizeof(TNo));
-        lst->prox = NULL;
-        while((fgets(string, 100, stdin)) != NULL) {
-            int balanceado = 1;
-        for (i = 0; string[i] != '\0'; i++) {
-            if (string[i] == '(')
-                balanceado = push (lst, string[i]);
-            if (string[i] == ')') {
-                balanceado = pop (lst);
-                if(balanceado == 0)
-                    break;
+#include <stdio.h>
+#include <string.h>
+int main(void){
+    char expressao[1001];
+    int verificador, i, j;
+
+    
+    while(fgets(expressao, 1000, stdin) != NULL){
+        verificador = 0;
+        for(j = 0; j < strlen(expressao); j++){
+            if(expressao[j] == '(')
+                verificador++;
+            else{
+                if(expressao[j] == ')'){
+                    verificador--;
+                    if(verificador < 0)
+                        break;
+                }
             }
         }
-    
-        if (balanceado == 1 && lst->prox == NULL)
+        if(verificador == 0)
             printf("correct\n");
-        else 
+        else
             printf("incorrect\n");
-        string[0] = '\0';
-    }
-        return 0;
+        expressao[0] = '\0';
+    }   
 
-        
-    }
+    return 0;
+}
