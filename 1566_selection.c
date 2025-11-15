4179833465b0a4cf8136f144f092cc7b0c52216c
joaouezu <joao.luz07@unifesp.br>
2025-11-15 11:47:00 -0300

    Update 1566_selection.c

--- 1566_selection.c
+++ 1566_selection.c
@@ -1,36 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-void selectionSort (int arr[], int N) {
-    int i, j, minimo;
-    for (i = 0; i < N - 1; i++) {
-        minimo = i;
-        for (j = i + 1; j < N; j++) {
-            if (arr[j] < arr[minimo])
-                minimo = j;
+/*Inserimos os elementos no vetor um de cada vez, ordenando-os em sua inserção
+Retirado também do livro Feofillof*/
+void insertionSort(int arr[], int N) {
+    int i, j, chave;
+    for (i = 1; i < N; i++) {
+        chave = arr[i];
+        j = i - 1;
+        
+        // Move elementos maiores que a chave para frente
+        while (j >= 0 && arr[j] > chave) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
         }
-        if (minimo != i) {
-            int temp = arr[minimo];
-            arr[minimo] = arr[i];
-            arr[i] = temp;
-        }
+        arr[j + 1] = chave;
     }
 }
-int main () {
+
+int main() {
     int NC, i, j, k;
-    do{
+    
+    do {
         scanf("%d", &NC);
     } while(NC < 0 || NC > 100);
+    
     for (i = 0; i < NC; i++) {
         int N;
         scanf("%d", &N);
         int arr[N];
+        
         for (j = 0; j < N; j++) {
-            scanf("%d", &arr[i]);
+            scanf("%d", &arr[j]);
         }
-            selectionSort (arr, N);
-            while (k = 0 < N) {
-                printf("%d", arr[k]);
-                k++;
-            }
+        
+        insertionSort(arr, N);
+        
+        for (k = 0; k < N; k++) {
+            printf("%d", arr[k]);
+            if (k < N - 1) printf(" ");
+        }
+        printf("\n");
     }
-    }
+    
+    return 0;
+}
