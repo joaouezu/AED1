d3e7fe7ab56a217261678ba31f34f499a9926ddd
joaouezu <joao.luz07@unifesp.br>
2025-11-15 12:00:24 -0300

    Update 1069.c

--- 1069.c
+++ 1069.c
@@ -6,21 +6,21 @@ int main () {
     int n;
     if(scanf("%d", &n) != 1)
         return 0;
-    getchar();
+    getchar(); // Limpeza de cache
     char string[1001];
     while (n-- > 0) {
     if (fgets(string, sizeof(string), stdin) == NULL)
         break;
         int verificar = 0, diamantes = 0, i;
-        for (i = 0; string[i] != '\0'; i++) {
+        for (i = 0; string[i] != '\0'; i++) { // Lendo string
             if (string[i] == '<')
-                verificar++;
+                verificar++; // incrementamos contador com '<'
             else 
                 if(string[i] == '>') {
                     if (verificar > 0) {
                         diamantes++;
                         verificar--;
-                    
+                    // Se tivermos um '<' e '>' na mesma pilha, decrescemos o contador e incrementamos o numero de diamantes
                 }
             }
         }
